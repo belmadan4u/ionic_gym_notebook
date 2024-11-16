209855539d224b82a869d27c066698fa842ff843
Abdou Belmadani <belmadani6655@gmail.com>
2024-11-16 18:46:04 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'carnet_muscu',
+  webDir: 'www'
+};
+
+export default config;
